5fc98bff2769ebd3c2dc70314354e82096de5110
1PatoRod <rpatricioesteban@gmail.com>
2023-03-13 19:17:10 -0300

    add src/models

---
+++ api/src/models/Size.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+
+//Export the function that defines the model.
+module.exports = (sequelize) => {
+    //Define model.
+    sequelize.define('Size', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        size: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },{
+        timestamps: false,
+        freezeTableName: true,
+    });
+}
