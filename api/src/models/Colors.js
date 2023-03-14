9ee9eac8905afd76050ee6782ab8fc3aac4795db
Franco-ortellado <ortellado.frank@gmail.com>
2023-03-14 19:34:38 -0300

    Cambios en los Models

--- api/src/models/Colors.js
+++ api/src/models/Colors.js
@@ -1,29 +1,33 @@
-const { DataTypes } = require("sequelize");
+const {DataTypes} = require('sequelize');
 
 //Export the function that defines the model.
 module.exports = (sequelize) => {
-  //Define model.
-  sequelize.define(
-    "Colors",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    },
-    {
-      timestamps: false,
-      freezeTableName: true,
-    }
-  );
+	//Define model.
+	sequelize.define(
+		'Colors',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				autoIncrement: true,
+				primaryKey: true,
+				allowNull: false,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			size: {
+				type: DataTypes.ARRAY,
+				allowNull: false,
+			},
+			stock: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+		},
+		{
+			timestamps: false,
+			freezeTableName: true,
+		}
+	);
 };
