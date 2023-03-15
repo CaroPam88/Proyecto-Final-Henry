f00278e5933ce8d16beb458154b0554f057b9a45
Franco-ortellado <ortellado.frank@gmail.com>
2023-03-15 17:59:34 -0300

    modularizo controller/baseDeDatos

--- api/src/basesDeDatos/dataGeneral.js
+++ api/src/basesDeDatos/dataGeneral.js
@@ -1,78 +1,102 @@
-const { Size, Colors } = require("../db");
+const {Size, Colors, Clothes} = require('../db');
 
 const sizeArray = [
-  {
-    size: "S",
-    stock: 1,
-  },
-  {
-    size: "M",
-    stock: 1,
-  },
-  {
-    size: "L",
-    stock: 1,
-  },
-  {
-    size: "XL",
-    stock: 1,
-  },
-  {
-    size: "XXL",
-    stock: 1,
-  },
+	{
+		size: 'S',
+		stock: 1,
+	},
+	{
+		size: 'M',
+		stock: 1,
+	},
+	{
+		size: 'L',
+		stock: 1,
+	},
+	{
+		size: 'XL',
+		stock: 1,
+	},
+	{
+		size: 'XXL',
+		stock: 1,
+	},
 ];
 
 const colorArray = [
-  {
-    color: "negro",
-    stockColor: 1,
-  },
-  {
-    color: "blanco",
-    stockColor: 1,
-  },
-  {
-    color: "azul",
-    stockColor: 1,
-  },
-  {
-    color: "amarillo",
-    stockColor: 1,
-  },
-  {
-    color: "rojo",
-    stockColor: 1,
-  },
+	{
+		color: 'negro',
+		stockColor: 1,
+	},
+	{
+		color: 'blanco',
+		stockColor: 1,
+	},
+	{
+		color: 'azul',
+		stockColor: 1,
+	},
+	{
+		color: 'amarillo',
+		stockColor: 1,
+	},
+	{
+		color: 'rojo',
+		stockColor: 1,
+	},
 ];
 
 const sizeBdd = async () => {
-  for (const iterator of sizeArray) {
-    await Size.findOrCreate({
-      where: {
-        size: iterator.size,
-        stockSize: iterator.stock,
-      },
-    });
-  }
+	for (const iterator of sizeArray) {
+		await Size.findOrCreate({
+			where: {
+				size: iterator.size,
+				stockSize: iterator.stock,
+			},
+		});
+	}
 
-  const allSize = await Size.findAll();
+	const allSize = await Size.findAll();
 
-  return allSize || "err";
+	return allSize || 'err';
 };
 
 const colorsBdd = async () => {
-  for (const iterator of colorArray) {
-    await Colors.findOrCreate({
-      where: {
-        color: iterator.color,
-        stockColor: iterator.stockColor,
-      },
-    });
-  }
+	for (const iterator of colorArray) {
+		await Colors.findOrCreate({
+			where: {
+				color: iterator.color,
+				stockColor: iterator.stockColor,
+			},
+		});
+	}
 
-  const allColors = await Colors.findAll();
+	const allColors = await Colors.findAll();
 
-  return allColors || "err";
+	return allColors || 'err';
 };
-module.exports = { sizeBdd, colorsBdd };
+
+const clothesBdd = async () => {
+	const clothesData = await Clothes.findAll({
+		include: [
+			{
+				model: Size,
+				attributes: ['size', 'stockSize'],
+				through: {
+					attributes: [],
+				},
+			},
+			{
+				model: Colors,
+				attributes: ['color', 'stockColor'],
+				through: {
+					attributes: [],
+				},
+			},
+		],
+	});
+
+	return clothesData;
+};
+
+module.exports = {sizeBdd, colorsBdd, clothesBdd};
