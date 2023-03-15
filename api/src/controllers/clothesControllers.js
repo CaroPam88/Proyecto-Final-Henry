004b9a2cf638961f4470fc1c1a0b71a58de5c8c8
Franco-ortellado <ortellado.frank@gmail.com>
2023-03-15 00:03:14 -0300

    modificacion en controller

--- api/src/controllers/clothesControllers.js
+++ api/src/controllers/clothesControllers.js
@@ -1,10 +1,10 @@
-const {Clothes, Colors} = require('../db');
+const {Clothes, Size} = require('../db');
 
 let getClothesData = async (searchClothe) => {
 	const clothesData = await Clothes.findAll({
 		include: {
-			model: Colors,
-			attributes: ['name', 'size', 'stock'],
+			model: Size,
+			attributes: ['size', 'colors', 'stock'],
 			through: {
 				attributes: [],
 			},
