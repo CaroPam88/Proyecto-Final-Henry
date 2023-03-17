90b67d6876024968d91fe71c725a5102657474e9
1PatoRod <rpatricioesteban@gmail.com>
2023-03-17 17:04:08 -0300

    modify filterBar/ actionFilter

--- client/src/Components/FilterBar/FilterBar.jsx
+++ client/src/Components/FilterBar/FilterBar.jsx
@@ -1,58 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import{ filterAllClothes } from "../../Redux/actionFilter"
 
 const Filter = () => {
   const dispatch = useDispatch();
-
+  
   // const filterClothes = useSelector((state) => state.products.filteredProducts);
-
+  
   const [filters, setFilters] = useState({
     genre: "",
-    filterBySize: "",
-    filterByColor: "",
-    filterByType: "",
+    size: "",
+    color: "",
+    type: "",
   });
 
-  function handlerFilter(event) {
-    setFilters({
-      ...filters,
-      [event.target.name]: event.target.value,
-    });
+  useEffect(() => {
     dispatch(
-      filterAllClothes({ ...filters, [event.target.value]: event.target.value })
+      filterAllClothes(filters)
+    );
+  }, [filters, dispatch])
+
+  console.log('state',filters);
+  function handlerFilter(event) {
+    setFilters({ ...filters,[event.target.name]: event.target.value });
+    dispatch(
+      filterAllClothes(filters)
     );
   }
+  // { ...filters, [event.target.value]: event.target.value }
 
   return (
     <div>
       <select name="genre" onChange={handlerFilter}>
         <option value="">---</option>
-        <option value="Female">Female</option>
-        <option value="Male">Male</option>
-       
+        <option value='Female'>Female</option>
+        <option value='Male'>Male</option>
+      
       </select>
 
       <select name="type" onChange={handlerFilter}>
         <option value="">---</option>
-        <option value="Remera">Remera</option>
-        <option value="Vestido">Vestido</option>
-        <option value="Chomba">Chomba</option>
-        <option value="Pantalón">Pantalón</option>
-        <option value="Short">Short</option>
+        <option value="remera">Remera</option>
+        <option value="vestido">Vestido</option>
+        <option value="chomba">Chomba</option>
+        <option value="pantalon">Pantalón</option>
+        <option value="short">Short</option>
       </select>
 
-      <select name="filterBySize" onChange={handlerFilter}>
+      <select name="size" onChange={handlerFilter}>
         <option value="">---</option>
         <option value="S">S</option>
-        <option value="l">L</option>
-        <option value="m">M</option>
-        <option value="xl">XL</option>
-        <option value="xxl">XXL</option>
+        <option value="L">L</option>
+        <option value="M">M</option>
+        <option value="XL">XL</option>
+        <option value="XXl">XXL</option>
       </select>
 
-      <select name="filterByColor" onChange={handlerFilter}>
+      <select name="color" onChange={handlerFilter}>
         <option value="">---</option>
         <option value="white">WHITE</option>
         <option value="black">BLACK</option>
--- client/src/Redux/actionFilter.js
+++ client/src/Redux/actionFilter.js
@@ -1,64 +1,40 @@
 import { filterClothes } from "./productSlice";
 
-const filterAllClothes = (state) => {
-  return (dispatch, getState) => {
-
-    const filterByGenre = getState().filteredProducts;
-
-  
-
+const filterAllClothes = (filters) => {
+  return async (dispatch, getState) => {
+    const {genre, size, color, type} = filters;
+    
     const allProducts = getState().products.products;
-    console.log(allProducts)
-    console.log(allProducts?.map((clothe) => clothe.sex.includes(state.genre)))
+    console.log('obtiene',allProducts)
+    // console.log('filtra',allProducts?.map(e => e.sex.filter((clothe) => clothe === genre)))
 
-    const clotheFilterByGenre =
-      state.sex === ""
-        ?  allProducts
-        : 
-        allProducts?.map((clothe) => clothe.sex.includes(state.genre));
-        
-        
+    const productsFilteredGenre = await genreFilter(genre, allProducts);
+    const productsFilteredType = await typeFilter(type, productsFilteredGenre);
+    const productsFilteredSize = await sizeFilter(size, productsFilteredType);
+    // const productsFilteredColor = await colorFilter(color, productsFilteredSize);
 
-    const clotheFilterByType = filterByType(
-      state.clotheFilterByType,
-      clotheFilterByGenre
-    );
-    const clotheFilterBySize = filterBySize(
-      state.clotheFilterBySize,
-      clotheFilterByType
-    );
-    const clotheFilterByColor = filterByColor(
-      state.clotheFilterByColor,
-      clotheFilterBySize
-    );
-    return dispatch(filterClothes(clotheFilterByColor))
+    return dispatch(filterClothes(productsFilteredSize))
   };
 };
 
-//cada vez que pase por un filtro sin importar la dirección, se va a copiar el estado anterior y no se van a pisar
-const filterByType = (type,  products)=>{
-    if(type === "") return  products;
-    else{
-        return  products.filter((c)=>c.type.find((t)=>t === type) )
-    }
-}
-//filtro primero por el type, donde voy a recorrer la ropa que tenga, la voy a mapear y
-//y buscar si hay types que coincidan en ella. Caso no se elija nada, devuelve toda la ropa
-const filterBySize= (size,  products) =>{
-    if(size === "") return  products;
-    else{
-        return  products.filter((c)=>c.size.find((s)=> s === size))
-    }
-}
-//idem con los talles, si viene vacio, retorno toda la ropa existente y de ahi la filtro para buscar los sizes
-//que coincidan
-
-const filterByColor =(color,  products)=>{
-    if(color === "") return  products;
-    else{
-        return  products.filter((c)=>c.color === color)
-    }
-}
-//en el caso de los colores lo que voy a hacer es buscar la coincidencia y mostrarlo, caso contrario, mostrara toda la ropa
+const genreFilter = (genre, allProducts) => {
+  const result = genre === ''
+    ? allProducts
+    : allProducts.filter(e => e.sex.includes(genre));
+  return result;
+};
+const typeFilter = (type, productsFilteredGenre) => {
+  const result = type === ''
+  ? productsFilteredGenre
+  : productsFilteredGenre.filter(cloth => cloth.type === type)
+  return result;
+};
+const sizeFilter = (size, productsFilteredType) => {
+  const result = size === ''
+  ? productsFilteredType
+  : productsFilteredType?.filter(cloth => cloth.size === size)
+  console.log(result);
+  return result;
+};
 
 export   {filterAllClothes};
