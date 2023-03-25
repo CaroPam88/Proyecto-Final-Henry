b8277b8f3a34120b1b4490620f948913da93d646
Franco-ortellado <ortellado.frank@gmail.com>
2023-03-25 16:57:07 -0300

    error ramdon que soy inmune

--- client/src/Redux/actionCart.js
+++ client/src/Redux/actionCart.js
@@ -1,14 +1,15 @@
-import { addToCart, getToCart } from "./cartAppSlice";
+import {addToCart, getToCart} from './cartAppSlice';
 
-const addCartProduct = ( product ) => {
-    return ( dispatch ) => {
-        return dispatch(addToCart(product));
-    }
+const addCartProduct = (product) => {
+	return (dispatch) => {
+		console.log(product);
+		return dispatch(addToCart(product));
+	};
 };
 const getCart = () => {
-    return async ( dispatch,getState ) => {
-        const userCart = getState().user.theUser.cart;
-        return dispatch(getToCart(userCart));
-    }
-}
-export { addCartProduct, getCart };
+	return async (dispatch, getState) => {
+		const userCart = getState().user.theUser.cart;
+		return dispatch(getToCart(userCart));
+	};
+};
+export {addCartProduct, getCart};
