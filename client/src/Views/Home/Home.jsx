767cd581b1609c12a3f195172b5d50a57da556a8
1PatoRod <rpatricioesteban@gmail.com>
2023-03-30 21:02:44 -0300

    carrito a mitad

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -7,17 +7,29 @@ import { useEffect } from "react";
 import CardContainer from "../../Components/CardContainer/CardContainer.jsx";
 import imgHome from '../../Assets/img/hero3.jpg';
 import  GenderCard  from "../../Components/Genders/GenderCards.jsx";
-// import { clearCurrentPurechase } from "../../Redux/actionCart.js";//importamos la action que limpia el producto o los productos comprado anteriormente con merccado pago.
+import { putClothes } from "../../Redux/ActionsGet.js";
 
 export const Home = ()=>{
-    // const theUser = useSelector(state => state.user.theUser);//Nos traemos user para despues limitar que solo se ejecute la funcion si estamos loggeados.
-    // const currentPurechase = useSelector(state => state.cart.currentPurchase);//Nos traemos el array con los productos comprados recientemente. Puede ser uno o mas pero, siempre en array.
-    // console.log(user);
-    const dispatch = useDispatch()
+
+    const theUser = useSelector(state => state.user.theUser);//Nos traemos user para despues limitar que solo se ejecute la funcion si estamos loggeados.
+    const currentPurechase = JSON.parse(localStorage.getItem('currentPurechase'));
+    const dispatch = useDispatch();
+
+    const update = window.location.href.includes("approved")
+    console.log('update',update);
+
+    const stockController = async () => {
+        if(theUser.id && update && currentPurechase){
+            await dispatch(putClothes(currentPurechase))
+            .then(window.localStorage.removeItem("currentPurechase"))//Coprobamos que exista el usuario preguntando por su propiedad id, ejecutamos la funcion que envia los productos comprados a la db, si esto funciona, se limpia lo/s ultimo/s productos comprados.
+        }
+    }
+
     useEffect (()=>{
         dispatch(getAllProducts())
-        // if(theUser.id) funcionDeUstedes(currentPurechase).then(() => dispatch(clearCurrentPurechase()))//Coprobamos que exista el usuario preguntando por su propiedad id, ejecutamos la funcion que envia los productos comprados a la db, si esto funciona, se limpia lo/s ultimo/s productos comprados.
-    }, [dispatch]);
+        stockController()
+        }, [dispatch, currentPurechase]);
+    console.log('currentPurechase',currentPurechase);
     return(
         <div className={style.container}>
                 <img src={imgHome} alt="" className={style.img} />
