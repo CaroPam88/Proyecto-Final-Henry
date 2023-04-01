f38773cf677fdca51e61632d29559c4dbb6d2402
1PatoRod <rpatricioesteban@gmail.com>
2023-04-01 12:56:15 -0300

    css adminProductsDetail

--- client/src/Views/Admin/Scenes/ViewProducts/DetailProduct/DetailProduct.jsx
+++ client/src/Views/Admin/Scenes/ViewProducts/DetailProduct/DetailProduct.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getProductDetail, clearProductDetailState } from "../../../../../Redux/ActionsGet";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import style from './DetailProduct.module.css';
+import hero3 from '../../../../../Assets/img/hero3.jpg';
 
 const DetailProduct = () => {
     const dispatch = useDispatch();
@@ -10,41 +13,65 @@ const DetailProduct = () => {
     useEffect(() => {
         dispatch(getProductDetail(id));
         return () => {
-          dispatch(clearProductDetailState());
+            dispatch(clearProductDetailState());
         };
-      }, []);
-      const detail = useSelector((state) => state.products.productDetail);
+    }, []);
+    const detail = useSelector((state) => state.products.productDetail);
     return (
         <section>
-            <section>
-                <img src={detail.image} alt={detail.name} />
-            </section>
-            <section>
-                <h3>Nombre: {detail.name}</h3>
-            </section>
-            <section>
-                <p>Tipo: {detail.type}</p>
-                <p>Precio: ${detail.price}</p>
-                <section>
-                    Genero: {detail.sex?.map((el, i) => <p key={i} >{el}</p>)}
+            <img src={hero3} alt='found' className={style.found} />
+            <section className={style.content}>
+                <section className={style.sectionOne}>
+                    <section className={style.imgContent}>
+                        <img src={detail.image} alt={detail.name} className={style.image} />
+                    </section>
+                    <section className={style.oneInfo}>
+                        <h3>Nombre: {detail.name}</h3>
+                        <p>Tipo: {detail.type}</p>
+                        <p>Precio: ${detail.price}</p>
+                    </section>
                 </section>
-                <p>Stock: {detail.stockGeneral}</p>
-                <section>
-                    {detail.sizes?.map((el, i) => {
-                        return <div key={i}>
-                            <p>Talle: {el.size}</p>
-                            <p>Stock por talle: {el.stockSize}</p>
-                            <section>
-                                <p>Colores del talle: </p>
-                                {el.colors?.map((col, i) => {
-                                    return <div key={i}>
-                                        <p>Color: {col.color}</p>
-                                        <p>Stock del color: {col.stockColors}</p>
-                                    </div>
-                                })}
-                            </section>
+                <section className={style.sectionTwo}>
+                <Link to='/admin/allProducts' className={style.return} >
+                    <h6>{'<<'}</h6>
+                </Link>
+                    <h3>Nombre: {detail.name}</h3>
+                    <section className={style.twoInfo}>
+                        <div className={style.genders}>
+                            <p>Genero: </p>
+                            {detail.sex?.map((el, i) => <p key={i} >{el}</p>)}
                         </div>
-                    })}
+                        <p>Stock: {detail.stockGeneral}</p>
+                        {detail.sizes?.map((el, i) => {
+                            return (<div key={i}>
+                                <p>Talle: {el.size} | Stock por talle: {el.stockSize}</p>
+                                <table className={style['colors-table']}>
+                                    <thead>
+                                        <tr>
+                                            <th>color</th>
+                                            <th>Stock del color</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {el.colors?.map((col, i) => {
+                                            return <tr key={i}>
+                                                <td>{col.color}</td>
+                                                <td>{col.stockColors}</td>
+                                            </tr>
+                                        })}
+                                    </tbody>
+                                </table>
+                            </div>)
+                        })}
+                    </section>
+                    <section className={style.buttonCont} >
+                        <div>Deshabilitado: </div>
+                        <label className={style.switch}>
+                            <input type="checkbox" className={style.checkbox}/>
+                            <div className={style.slider}></div>
+                        </label>
+                        <button className={style.button} >modificar</button>
+                    </section>
                 </section>
             </section>
         </section>
