75691f3801d2b7dc25945ae5327f20ea43244e23
Franco-ortellado <ortellado.frank@gmail.com>
2023-04-04 15:41:44 -0300

    cambios en el componente perfil

--- client/src/Authentication/Components/LogoutButton.jsx
+++ client/src/Authentication/Components/LogoutButton.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import style from "../Components/LogoutButton.module.css"
+import React from 'react';
+import {useAuth0} from '@auth0/auth0-react';
+import style from '../Components/LogoutButton.module.css';
 
 const LogoutButton = () => {
-  const { logout } = useAuth0();
-  return (
-    <div>
-      <button
-        className={style.button}
-        type="button"
-        onClick={() => {
-          window.localStorage.removeItem("userStorage");
-          logout({ returnTo: '/' });
-        }}
-      >
-      Logout
-      </button>
-    </div>
-  );
+	const {logout} = useAuth0();
+	return (
+		<div>
+			<button
+				className={style.button}
+				type="button"
+				onClick={() => {
+					window.localStorage.removeItem('userStorage');
+					logout({returnTo: '/'});
+				}}
+			>
+				Log Out
+			</button>
+		</div>
+	);
 };
 
 export default LogoutButton;
